90384d061463c7127873f5a691481f9ab6896265
Joshua Carey <jdcarey128@gmail.com>
2021-03-29 09:31:11 -0600

    Setup app configuration

---
+++ api/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask 
+from flask_sqlalchemy import SQLAlchemy 
+
+# local imports 
+from config import config 
+
+db = SQLAlchemy()
+
+def create_app(config_name = 'default'): 
+  app = Flask(__name__)
+
+  @app.route('/')
+  def index(): 
+    return 'Hello Joshua'
+
+  return app 
