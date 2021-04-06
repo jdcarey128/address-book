0f2dc9ffd76d81a6d301cf892179c5a88cc2e128
Joshua Carey <jdcarey128@gmail.com>
2021-04-06 10:12:55 -0600

    add manage.py file for migrations

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ from api import create_app
 runner file to create application api 
 '''
 
-config_name = os.getenv('FLASK_CONFIG')
+config_name = os.environ.get('FLASK_CONFIG')
 app = create_app(config_name)
 
 if __name__ == '__main__': 
